2919df6eb86fe769999de909b1ba2523a1e4ef48
wdNaimur <wd.naimur@gmail.com>
2025-10-02 20:37:11 +0600

    feat: navbar add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/shared/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+        <main className="container px-4">{children}</main>
       </body>
     </html>
   );
